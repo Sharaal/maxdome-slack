6782d4ee0fc4b0924ac0fe0d16d676fc3842349c
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-07-13 14:38:52 +0200

    Formatting the results

--- src/commands/mxd-search.js
+++ src/commands/mxd-search.js
@@ -1,7 +1,8 @@
 module.exports = ({ heimdall }) => async ({ args, reply }) => {
-  const assets = await heimdall.search(args);
-  if (assets.length) {
-    reply.text(assets.join(', '));
+  const lines = await heimdall.search(args);
+  if (lines.length) {
+    lines.push(`<https://http://store.maxdome.de/suche?search=${args}|show all...>`);
+    reply.text(lines.join('\n'));
   } else {
     reply.text(`no results found for "${args}"`);
   }
