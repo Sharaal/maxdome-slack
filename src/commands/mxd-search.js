b9de93c31bf5104a571443b14ddcd1fedcf76fb0
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-07-13 14:40:51 +0200

    Fix broken link

--- src/commands/mxd-search.js
+++ src/commands/mxd-search.js
@@ -1,7 +1,7 @@
 module.exports = ({ heimdall }) => async ({ args, reply }) => {
   const lines = await heimdall.search(args);
   if (lines.length) {
-    lines.push(`<https://http://store.maxdome.de/suche?search=${args}|show all...>`);
+    lines.push(`<https://store.maxdome.de/suche?search=${args}|show all...>`);
     reply.text(lines.join('\n'));
   } else {
     reply.text(`no results found for "${args}"`);
