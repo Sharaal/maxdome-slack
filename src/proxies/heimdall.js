47519b6deca339695d8067cd93a93d4cb431a8da
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-07-15 10:16:35 +0200

    Refactor to be able to reuse the common logic

--- src/proxies/heimdall.js
+++ src/proxies/heimdall.js
@@ -4,16 +4,17 @@ const AssetsQuery = require('./assets-query.js');
 
 module.exports = ({ apikey, appid, proxyOptions = {} }) => {
   return {
-    search: (term, searchOptions = {}) => {
-      const options = Object.assign({}, proxyOptions.searchOptions || {}, searchOptions);
+    query(options) {
       const query = (new AssetsQuery())
         .query('apikey', apikey)
         .query('appid', appid)
-        .filter('contentTypeSeriesOrMovies')
-        .filter('search', term);
+        .filter('contentTypeSeriesOrMovies');
       if (options.pageSize) {
         query.query('pageSize', options.pageSize);
       }
+      return query;
+    },
+    assets(query) {
       return rp.get({
         url: `https://heimdall.maxdome.de/interfacemanager-2.1/mxd/assets?${query}`,
         headers: {
@@ -24,6 +25,12 @@ module.exports = ({ apikey, appid, proxyOptions = {} }) => {
         json: true,
         transform: data => data.assetList.map(asset => `${asset.title} http://store.maxdome.de/${asset.id}`)
       });
+    },
+    search(term, searchOptions = {}) {
+      const options = Object.assign({}, proxyOptions.searchOptions || {}, searchOptions);
+      const query = this.query(options)
+        .filter('search', term);
+      return this.assets(query);
     }
   }
 };
