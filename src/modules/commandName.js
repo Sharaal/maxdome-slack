ba167be8d43ca5345a2526bc8b799b5756424613
Christoph Herrmann <christoph.herrmann@maxdome.de>
2016-09-22 12:18:53 +0200

    Also allow to send commands by interactive buttons

--- src/modules/commandName.js
+++ src/modules/commandName.js
@@ -1,3 +1,8 @@
 module.exports = ({ req }) => {
-  return { commandName: req.body.command, args: req.body.text };
+  const body = req.body;
+  if (body.actions && body.actions.length > 0) {
+    const action = body.actions[0];
+    return { commandName: action.name, args: action.value };
+  }
+  return { commandName: body.command, args: body.text };
 };
